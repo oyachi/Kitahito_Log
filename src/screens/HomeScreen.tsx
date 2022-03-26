b85c4cc8dd12dfcfdc20835226df9fb9e116b26c
Daichi <ikakarasu714@gmail.com>
2022-03-27 04:23:32 +0900

    Update: responsive

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -6,6 +6,8 @@ import QRViewer from '../components/QRViewer';
 /* styles */
 import styles from '../styles/HomeScreen.module.css';
 import { useTheme } from '@mui/material/styles';
+/* responsive */
+import MediaQuery from 'react-responsive';
 
 const InputScreen: React.VFC = () => {
   const theme = useTheme();
@@ -24,11 +26,16 @@ const InputScreen: React.VFC = () => {
 
   return (
     <div className={styles.background}>
-      <Box bgcolor={theme.palette.form.main} borderRadius={'10px'} className={styles.form_area}>
-        {/*<div className={styles.form_area}>*/}
-        {isQrGenerate ? <QRViewer str={visitor} reinput={reinput} /> : <FormArea updateVisitor={updateVisitor} />}
-        {/*</div>*/}
-      </Box>
+      <MediaQuery query="(max-width: 767px)">
+        <Box bgcolor={theme.palette.form.main} borderRadius={'10px'} className={styles.form_area}>
+          {isQrGenerate ? <QRViewer str={visitor} reinput={reinput} /> : <FormArea updateVisitor={updateVisitor} />}
+        </Box>
+      </MediaQuery>
+      <MediaQuery query="(min-width: 768px)">
+        <Box bgcolor={theme.palette.form.main} borderRadius={'10px'} sx={{ width: 500 }} className={styles.form_area}>
+          {isQrGenerate ? <QRViewer str={visitor} reinput={reinput} /> : <FormArea updateVisitor={updateVisitor} />}
+        </Box>
+      </MediaQuery>
     </div>
   );
 };
